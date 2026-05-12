bf02f6043c43213dde880314313f81c07e7bbaf4
MaximeWlc <max46.welsch@gmail.com>
2026-05-12 10:44:40 +0200

    Update qcm.h

--- qcm.h
+++ qcm.h
@@ -1,7 +1,19 @@
+/*
+ * Description : Fichier d'en-tete contenant les definitions de constantes,
+ * les structures de donnees principales (Question et QCM)
+ * des fonctions utilisees dans le logiciel "Questions pour un Tekien".
+ */
+
+/* Gardiens d'inclusion pour eviter les erreurs de redefinition si le fichier est inclus plusieurs fois */
 #ifndef QCM_H
 #define QCM_H
 
 #define PASS_PROF "cytech2026"
+
+/*
+ * Structure representant une question individuelle du QCM.
+ * Contient l'enonce, les 4 propositions de reponse et la ou les bonnes reponses attendues.
+ */
 
 typedef struct {
     char intitule[500];
@@ -12,6 +24,12 @@ typedef struct {
     char bonnes_reponses[5]; 
 } Question;
 
+/*
+ * Structure representant un QCM complet.
+ * Regroupe les parametres de jeu (points negatifs, choix multiples, mode sequentiel)
+ * et un tableau de 10 questions pretes a etre posees.
+ */
+
 typedef struct {
     char titre[100];
     int points_negatifs; 
@@ -21,6 +39,8 @@ typedef struct {
 } QCM;
 
 /* Prototypes des fonctions */
+
+/* Vide le buffer d'entree clavier pour eviter les boucles infinies lors de saisies invalides */
 void vider_buffer();
 void sauvegarder_qcm(QCM q);
 int charger_qcm(char *nom_qcm, QCM *q);
